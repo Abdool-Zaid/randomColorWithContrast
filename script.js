5147a8c362589686c369bd58c1496bfcad9b78bd
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-11-13 12:16:32 +0200

    'stable'

--- script.js
+++ script.js
@@ -5,15 +5,14 @@ let generateRandomColor = (contrast, id, clearVal) => {
   let Contrasting="#";
   let outPut;
   let temp;
-  
   let i;
   for (i = 0; i < 6; i++) {
     temp = letters[Math.floor(Math.random() * 16)];
     color += temp;
     let index = letters.split('').findIndex((item) => item == temp);
-      Contrasting+=letters[16-index]
+    Contrasting+=letters[15-index]
   }
-  
+
   contrast == true ? (outPut = color) : (outPut = Contrasting);
 //   console.log(outPut)
   return outPut;
@@ -25,8 +24,6 @@ aspect-ratio:1;
     background-color:${generateRandomColor(true, "firstDiv")}
 `;
   document.querySelector("#div2").style = `
-width:400px;
-aspect-ratio:1;
-    background-color:${generateRandomColor(false, "firstDiv", true)}
+  background-color:${generateRandomColor(false, "firstDiv", true)}
 `;
 });
